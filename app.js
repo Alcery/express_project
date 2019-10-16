6f7f03bf7769bcd9c1c77c34681cb7a78ebc70f0
Nicholas Le <nle031@west-mec.org>
2019-10-16 16:46:19 -0700

    added api code snippet

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const fetch = require('node-fetch');
 const path = require('path');
 var express = require('express');
 var app = express();
+var unirest = require("unirest");
 const port = 8080;
+
+
+var req = unirest("GET", "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf");
+
+req.headers({
+	"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+	"x-rapidapi-key": "4a0a9d7d1cmshd592e3155a948f3p1ee0dfjsn57de095afac7"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
 
 app.set('view engine', 'pug');
