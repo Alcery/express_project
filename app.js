1495c1150e46819dcb588dae0ba559bdd65955f4
Nicholas Le <nle031@west-mec.org>
2019-10-17 16:07:56 -0700

    Added js

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ const fetch = require('node-fetch');
 const path = require('path');
 var express = require('express');
 var app = express();
-var unirest = require("unirest");
+var request = require("request");
 const port = 8080;
 
 
-var req = unirest("GET", "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf");
-
-req.headers({
-	"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-	"x-rapidapi-key": "4a0a9d7d1cmshd592e3155a948f3p1ee0dfjsn57de095afac7"
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+var options = {
+    method: 'GET',
+    url: 'https://wordsapiv1.p.rapidapi.com/words/bellwether',
+    headers: {
+      'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+    }
+  };
+  
+  request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+  
+      console.log(body);
+  });
 
 app.set('view engine', 'pug');
+app.set("views", path.join(__dirname,"views"));
+
+app.use("/static", express.static(path.join(__dirname,"public")));
