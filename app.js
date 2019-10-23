8946034cddff90aaa266f89daf69aa75851a556c
Nicholas Le <nle031@west-mec.org>
2019-10-22 17:09:02 -0700

    got nav links to link up

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var app = express();
 
 const port = 8080;
 var url = "https://wordsapiv1.p.rapidapi.com/words/sesquipedalian/definitions"
+var url2 = "https://wordsapiv1.p.rapidapi.com/words/Perfidiousness/definitions"
+var url3 = "https://wordsapiv1.p.rapidapi.com/words/Penultimate/definitions"
+var url4 = "https://wordsapiv1.p.rapidapi.com/words/Circumlocution/definitions"
+var url5 = "https://wordsapiv1.p.rapidapi.com/words/Bellwether/definitions"
 
 
 app.set('view engine', 'pug');
@@ -20,6 +24,46 @@ app.get('/', (req, res) => {
   .then(
     (json) => {
       res.render('index', {
+        word: json
+      })
+    })
+  });
+app.get('/2', (req, res) => {
+ fetch(url2)
+  .then(res => res.json())
+  .then(
+    (json) => {
+      res.render('2', {
+        word: json
+      })
+    })
+  });
+app.get('/3', (req, res) => {
+ fetch(url3)
+  .then(res => res.json())
+  .then(
+    (json) => {
+      res.render('3', {
+        word: json
+      })
+    })
+  });
+app.get('/4', (req, res) => {
+ fetch(url4)
+  .then(res => res.json())
+  .then(
+    (json) => {
+      res.render('4', {
+        word: json
+      })
+    })
+  });
+app.get('/5', (req, res) => {
+ fetch(url5)
+  .then(res => res.json())
+  .then(
+    (json) => {
+      res.render('5', {
         word: json
       })
     })
